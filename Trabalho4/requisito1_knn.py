b77474c073a9662602a1a0e73c638b869e4bf7bd
tiagogallo <tiagodecarvalhopereira@gmail.com>
2018-05-13 12:34:10 -0300

    knn implementado

--- Trabalho4/requisito1_knn.py
+++ Trabalho4/requisito1_knn.py
@@ -1,12 +1,12 @@
 import numpy as np
 import cv2
 import os
+import time
 
-def classify(centroPele, centroFundo, Ponto):
-    distPele = ((Ponto[0] - centroPele[0])**2 + (Ponto[1] - centroPele[1])**2 + (Ponto[2] - centroPele[2])**2) ** (1/2)
-    distFundo = ((Ponto[0] - centroFundo[0])**2 + (Ponto[1] - centroFundo[1])**2 + (Ponto[2] - centroFundo[2])**2) ** (1/2)
-
-    if distPele < distFundo:
+def classify(knn, Ponto):
+    ret, results, neighbours ,dist = knn.findNearest(Ponto, 3)
+    
+    if results == [1]:
         return 1
     else:
         return 0
@@ -30,7 +30,7 @@ for image in filelist:
 
 threshold = 27
 imgNumber = 0
-for og_image, gt_image in zip(original_list, gt_list):
+for og_image, gt_image in zip(original_list[:-2], gt_list[:-2]):
     img_og = cv2.imread(og_image)
     img_gt = cv2.imread(gt_image)
 
@@ -58,13 +58,15 @@ for og_image, gt_image in zip(original_list, gt_list):
 
     #print("List of ground truths = {} and shape = {}".format(list_for_gt, list_for_gt.shape))
 
-    Z = np.hstack([Z_og, list_for_gt])
+    #Z = np.hstack([Z_og, list_for_gt])
     #print("final data shape = ", Z.shape)
 
     if imgNumber == 0:
-        finalData = Z
+        finalData = Z_og
+        finalLabels = list_for_gt
     else:
-        finalData = np.vstack([finalData, Z])
+        finalData = np.vstack([finalData, Z_og])
+        finalLabels = np.vstack([finalLabels, list_for_gt])
 
     #cv2.imshow("original", img_og)
     #cv2.imshow("groundTruth", img_gt)
@@ -75,39 +77,48 @@ for og_image, gt_image in zip(original_list, gt_list):
 
 #print ("FinalData shape = ", finalData.shape)
 finalData = np.float32(finalData)
+finalLabels = np.float32(finalLabels)
 
 
-# define criteria and apply kmeans()
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-ret,label,center=cv2.kmeans(finalData,2,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
 
-print ("centers = ", center)
+knn = cv2.ml.KNearest_create()
+knn.train(finalData, cv2.ml.ROW_SAMPLE, finalLabels)
 
-centerFundo = center[0]
-centerPele = center[1]
-
-centerFundo = centerFundo[:-1]
-centerPele = centerPele[:-1]
-
-print("Centro da pele = {}\nCentro do fundo = {}".format(centerPele, centerFundo))
-
-
-og_test = original_list[0]
-gt_test = gt_list[0]
+og_test = original_list[-1]
+gt_test = gt_list[-1]
 
 og_img_test = cv2.imread(og_test)
 gt_img_test = cv2.imread(gt_test)
 
 classified = og_img_test.copy()
 
+Num_a_processar = og_img_test.shape[0] * og_img_test.shape[1]
+Num_processado = 0
+start_time = time.time()
+i = 5
 for w in range(og_img_test.shape[0]):
     for h in range(og_img_test.shape[1]):
-        cor = og_img_test[w][h][:]
-        label = classify(centerPele, centerFundo, cor)
-        if label == 0:
-            classified[w][h][:] = (255,255,255)
-        else:
+        if i % 5 != 0:
+            Num_processado += 1
             classified[w][h][:] = (0,0,0)
+            i += 1
+            continue
+
+        cor = og_img_test[w][h][:]
+        cor = np.expand_dims(cor, axis=1)
+        cor = cor.reshape((-1,3))
+        #print("cor shape = ", cor.shape)
+        label = classify(knn, np.float32(cor))
+        if label == 0:
+            classified[w][h][:] = (0,0,0)
+        else:
+            classified[w][h][:] = (255,255,255)
+
+        if Num_processado == 0 or Num_processado % 3900 == 0:
+            print("Passo demorou: {:.2f}seg.\tJa foram processados {} pontos de um total de {} pontos ===> {:.2f}%".format(time.time() - start_time,Num_processado, Num_a_processar, (Num_processado/Num_a_processar)*100))
+            start_time = time.time()
+        Num_processado +=1
+        i += 1
 
 cv2.imshow("imagem original", og_img_test)
 cv2.imshow("Ground Truth", gt_img_test)
