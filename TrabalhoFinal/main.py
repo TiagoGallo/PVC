a2383bfd29d74bb1184a8ea7d51ef8e30ab8b740
tiagogallo <tiagodecarvalhopereira@gmail.com>
2018-06-29 01:21:08 -0300

    Implement mouse movements

--- TrabalhoFinal/main.py
+++ TrabalhoFinal/main.py
@@ -2,6 +2,7 @@ import cv2
 import face_recognition
 import argparse
 import numpy as np
+import pyautogui
 
 # landmark color mapping for drawing
 LANDMARK_COLOR_MAP = {
@@ -74,15 +75,92 @@ def user_state(img, landmarks):
     # determine if there should be movement
     print('[DEBUG] pixel diff: ({}, {}/{})'.format(x_diff, y_diff_eye, y_diff_nose))
     state = [0, 0]
-    if x_diff < -10:
+    #Movimento para direita
+    if x_diff < -5:
+        print("[DEBUG] Direita")
         state[0] = -1
+    #Movimento para esquerda
     elif x_diff > 10:
+        print("[DEBUG] Esquerda")
         state[0] = 1
-    if y_diff_eye < 10:
+    #movimento para baixo
+    if y_diff_eye < 15:
+        print("[DEBUG] Baixo")
         state[1] = -1
+    #Movimento para cima
     elif y_diff_nose < 40:
+        print("[DEBUG] Cima")
         state[1] = 1
     return state
+
+class Mouse:
+    def __init__(self, args):
+        self.width, self.height = pyautogui.size()
+        print("[DEBUG] O tamanho da janela eh {}x{}".format(self.width, self.height))
+
+        # Create an accelarator for both directions and a max value for them
+        self.accX   = 0
+        self.accY   = 0
+        self.accMax = 10
+
+        # Delay to click the mouse
+        self.delay = args["delay"]
+
+    def move(self, state):
+        '''
+        Receive the program state as [X, Y]
+            X: -1 = go right    0 = nothing     1 = go left
+            Y: -1 = go down     0 = nothing     1 = go up
+        '''
+        X, Y = state
+        
+        w_pos, h_pos = self.actual_position()
+
+        # Analyze the x movement
+        if X == 0:
+            self.att_acc('X')    #decrease the acc if different of 0
+        elif X == -1:
+            self.accX += 1 # Move right
+            if self.accX > self.accMax: self.accX = self.accMax # Limit the moviment
+        elif X == 1:
+            self.accX -= 1 # Move left
+            if self.accX < -(self.accMax): self.accX = -(self.accMax) # Limit the moviment
+
+        # Analyze the y movement
+        if Y == 0:
+            self.att_acc('Y')    #decrease the acc if different of 0
+        elif Y == -1:
+            self.accY += 1 # Move down
+            if self.accY > self.accMax: self.accY = self.accMax # Limit the moviment
+        elif Y == 1:
+            self.accY -= 1 # Move up
+            if self.accY < -(self.accMax): self.accY = -(self.accMax) # Limit the moviment
+
+        pyautogui.moveRel(self.accX, self.accY)
+
+    def actual_position(self):
+        '''
+        Get mouse's actual position
+        '''
+        w_pos, h_pos = pyautogui.position()
+
+        return [w_pos, h_pos]
+
+    def att_acc(self, axis):
+        '''
+        Method to desaccelerate the accelerator
+        '''
+        if axis == 'X':
+            if self.accX > 0:
+                self.accX -= 1
+            elif self.accX < 0:
+                self.accX += 1
+
+        if axis == 'Y':
+            if self.accY > 0:
+                self.accY -= 1
+            elif self.accY < 0:
+                self.accY += 1
 
 def main():
     """
@@ -105,6 +183,9 @@ def main():
 
     # create the capture object with the default webcam
     cam = cv2.VideoCapture(0)
+
+    # create the mouse object
+    mouse = Mouse(args)
 
     # counter for skipping frames
     counter = 0
@@ -140,7 +221,7 @@ def main():
                     state = user_state(img, landmarks)
 
                     # move mouse according to state
-                    pass
+                    mouse.move(state)
 
                     # draw the bounding box and the landmarks on the frame
                     draw_bbox(img, location)
@@ -157,19 +238,6 @@ def main():
 
     cam.release()
     cv2.destroyAllWindows()
-
-
-def calc_relative_nose(face_locations, face_landmarks_list):
-    for face_location, landmark in zip(face_locations, face_landmarks_list):
-        point_med_nose = [0,0]
-        for i, point in enumerate(landmark["nose_tip"]):
-            point_med_nose[0] = point_med_nose[0] + point[0]
-            point_med_nose[1] = point_med_nose[1] + point[1]
-
-        point_med_nose[0] = point_med_nose[0] // i
-        point_med_nose[1] = point_med_nose[1] // i
-        print("[DEBUG] Ponto medio do nariz = ", point_med_nose)
-
 
 if __name__ == "__main__":
     main()
